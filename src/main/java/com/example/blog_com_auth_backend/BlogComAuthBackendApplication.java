011ca688e9b0e728abe8007a65ab790b84ab2476
Micael Machado <micarique2010@gmail.com>
2025-05-06 16:46:46 -0300

    feat: inicialização do projeto com dependencias base + inclusão da dependencia do JWT

---
+++ src/main/java/com/example/blog_com_auth_backend/BlogComAuthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog_com_auth_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogComAuthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogComAuthBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blog_com_auth_backend/BlogComAuthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blog_com_auth_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogComAuthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
