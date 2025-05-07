ee6aede1fd94c6db42b7b7b1d3c71b12333fff3e
Micael Machado <micarique2010@gmail.com>
2025-05-07 19:03:14 -0300

    refactor: Implementação do DTO no PostController para evitar a exposição da entidade

--- src/main/java/com/example/blog_com_auth_backend/controller/PostController.java
+++ src/main/java/com/example/blog_com_auth_backend/controller/PostController.java
@@ -1,6 +1,6 @@
 package com.example.blog_com_auth_backend.controller;
 
-import com.example.blog_com_auth_backend.dto.PostDTO;
+import com.example.blog_com_auth_backend.dto.PostRequestDTO;
 import com.example.blog_com_auth_backend.dto.PostResponseDTO;
 import com.example.blog_com_auth_backend.model.Post;
 import com.example.blog_com_auth_backend.service.PostService;
@@ -28,25 +28,25 @@ public class PostController {
 
     // Endpoint público para obter post por ID
     @GetMapping("/{id}")
-    public ResponseEntity<Post> searchPost(@PathVariable Long id) {
+    public ResponseEntity<PostResponseDTO> searchPost(@PathVariable Long id) {
         return postService.searchPost(id);
     }
 
-    // Endpoint privado para criar post (requer autenticação)
+    // Endpoint para criar post
     @PostMapping
-    public ResponseEntity<Post> createPost(@RequestBody @Valid PostDTO postDTO, @AuthenticationPrincipal UserDetails userDetails) {
-        return postService.createPost(postDTO, userDetails);
+    public ResponseEntity<PostResponseDTO> createPost(@RequestBody @Valid PostRequestDTO postRequestDTO, @AuthenticationPrincipal UserDetails userDetails) {
+        return postService.createPost(postRequestDTO, userDetails); // Certifique-se de retornar PostResponseDTO aqui
     }
 
-    // Atualizar post (autenticado)
+    // Endpoint para atualizar post
     @PutMapping("/{id}")
-    public ResponseEntity<?> updatePost(@PathVariable Long id, @RequestBody @Valid PostDTO postDTO, @AuthenticationPrincipal UserDetails userDetails) {
-        return postService.updatePost(id, postDTO, userDetails);
+    public ResponseEntity<PostResponseDTO> updatePost(@PathVariable Long id, @RequestBody @Valid PostRequestDTO postRequestDTO, @AuthenticationPrincipal UserDetails userDetails) {
+        return postService.updatePost(id, postRequestDTO, userDetails); // Certifique-se de retornar PostResponseDTO aqui
     }
 
     // Deletar post (autenticado)
     @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deletePost(@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseEntity<Void> deletePost(@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails) {
         return postService.deletePost(id, userDetails);
     }
 }
--- src/main/java/com/example/blog_com_auth_backend/dto/PostRequestDTO.java
+++ src/main/java/com/example/blog_com_auth_backend/dto/PostRequestDTO.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-public class PostDTO {
+public class PostRequestDTO {
 
     @NotBlank
     private String titulo;
