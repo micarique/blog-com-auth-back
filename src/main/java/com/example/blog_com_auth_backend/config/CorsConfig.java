dbc2610438696c1478b04a72ef6460a4c98c627d
Micael Machado <micarique2010@gmail.com>
2025-05-08 12:25:26 -0300

    feat: Configuração do cors para comunicação com o frontend

---
+++ src/main/java/com/example/blog_com_auth_backend/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.blog_com_auth_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // permite todos os endpoints
+                        .allowedOrigins("http://localhost:3000") // origem do seu front-end
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
